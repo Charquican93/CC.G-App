79d2f5623b49b3dbcb371ca977f5b06e3737770d
Leonardo A Gonzalez C <leonardo-gonzalez@live.cl>
2026-01-29 16:48:41 -0300

    Modificada la URL de la API

--- config.ts
+++ config.ts
@@ -1,1 +1,2 @@
-export const API_URL = 'http://192.168.199.219:3001';
+// export const API_URL = 'http://192.168.199.219:3001';
+export const API_URL = 'ccg-app-production.up.railway.app'; // Pega aquí la URL del Paso 2 (sin barra al final)
